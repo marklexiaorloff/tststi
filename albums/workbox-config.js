9bbc2fd0195ac3ba75cee4d1f79384affb97ae9f
marklexiaorloff <42719863+marklexiaorloff@users.noreply.github.com>
2018-10-31 05:41:22 +0800

    1

---
+++ albums/workbox-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "img/**.*",
+    "offline.html",
+    "icons/**.*"
+  ],
+  "swSrc": "src/sw.js",
+  "swDest": "sw.js",
+  "globIgnores": [
+    "./workbox-cli-config.js",
+	"./css/amp.css"
+  ]
+};
