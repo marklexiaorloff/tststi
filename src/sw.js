f3d5ec76ec148a21f0acab7d32a650839d127bc1
marklexiaorloff <42719863+marklexiaorloff@users.noreply.github.com>
2018-11-21 02:47:50 +0800

    1

--- src/sw.js
+++ src/sw.js
@@ -4,8 +4,8 @@ workbox.precaching.precacheAndRoute([]);
 
 self.addEventListener('install', event => {
   const urls = [
-    'https://cdn.ampproject.org/v0.js',
-    'https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js',
+    'https://lagunalions.com/js/v0.js',
+    'https://lagunalions.com/js/amp-install-serviceworker-0.1.js',
     'https://cdn.ampproject.org/shadow-v0.js',
     'index.html',
     '/'
